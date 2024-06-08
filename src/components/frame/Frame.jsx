f2ff4c9a0440c62175f9aa5c82a9c3a191a5e2c6
Alper <fod.zeus1@gmail.com>
2024-06-08 19:04:08 +0300

    finished

--- src/components/frame/Frame.jsx
+++ src/components/frame/Frame.jsx
@@ -1,24 +1,101 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './frame.css'
+import { createWorker } from 'tesseract.js'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 const Frame = () => {
 
   const [selectedImage, setSelectedImage] = useState(null);
+  const [textResult, setTextResult] = useState("Upload file to convert text.");
+  const [copyT, setCopyT] = useState("Copy");
+  const [progress, setProgress] = useState(0);
+
+  function handleImg (e) {
+    setSelectedImage(e.target.files[0]);
+  }
+
+  async function convertImgToText(image) {
+    const worker = createWorker({
+      logger: m => {
+        if (m.status === 'recognizing text') {
+          let x = m.progress * 100;
+          setProgress(x);
+        }
+      }
+    });
+    
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    
+    const { data: { text } } = await worker.recognize(image);
+    
+    await worker.terminate();
+    return text;
+  }
+
+  useEffect(() => {
+    console.log(`Progress: ${progress}%`);
+  }, [progress]);
+
+  useEffect(() => {
+    (async () => {
+      if (selectedImage) {
+        setTextResult("Uploading...");
+        setProgress(0); 
+        try {
+          const text = await convertImgToText(selectedImage);
+          console.log(text);
+          setTextResult(text);
+        } catch (error) {
+          console.error("Error converting image to text:", error);
+          setTextResult("Error converting image to text.");
+        }
+      }
+    })();
+  }, [selectedImage])
+
+  function copyText(){
+    navigator.clipboard.writeText(textResult);
+    setCopyT("Copied");
+    setTimeout(() => {
+      setCopyT("Copy");
+    }, 2000);
+  }
 
   return (
     
     <div className='frame'>
-      
       <div className="container">
         <div className="title">Img To Text Converter</div>
-
-        <label htmlFor="upload" className='btn'>Upload Image</label>
-        <input type="file" id='upload' className="btn"/>  
+        <div className="desc">Created by Alper Ertugrul</div> 
         
         <div className="bottom">
-          <div className="imgcontainer"><img src="./a.jpg" alt="" /></div>
-          <div className="textcontainer">Up unpacked friendly ecstatic so possible humoured do. Ample end might folly quiet one set spoke her. We no am former valley assure. Four need spot ye said we find mile. Are commanded him convinced dashwoods did estimable forfeited. Shy celebrated met sentiments she reasonably but. Proposal its disposed eat advanced marriage sociable. Drawings led greatest add subjects endeavor gay remember. Principles one yet assistance you met impossible.</div>
+          <div className="left">
+            <img src={selectedImage ? URL.createObjectURL(selectedImage) : "/test2.png"} alt="" className='img'/>
+            <label htmlFor="upload" className='btn'>Upload Image</label>
+            <input type="file" id='upload' accept="image/*" onChange={handleImg} className="btn"/> 
         </div>
+        
+
+          
+        <div className="right">
+
+          {selectedImage && progress > 0 && progress < 100 && (
+            <div className="progress-bar-container">
+              <div className='progress-bar-text'>{Math.floor(progress)} %</div>
+              <div className="progress-bar" style={{width:`${progress}%`,height:"35px"}}></div>
+            </div>
+          )}
+
+            <div className="text">{textResult}</div>
+            <div className="copybtn" id='copybtn' onClick={copyText}><ContentCopyIcon/> {copyT}</div>
+          </div>
+        </div>
+
+        
+        
+     
       </div>  
     </div>
   )
